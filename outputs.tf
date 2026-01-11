d59fd3d5b5604e63fa69fc6d84242e75695023ac
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:46:08 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "mssql_job_agents" {
+  description = "All mssql_job_agent resources"
+  value       = azurerm_mssql_job_agent.mssql_job_agents
+}
+
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "mssql_job_agents" {
+  description = <<EOT
+Map of mssql_job_agents, attributes below
+Required:
+    - database_id
+    - location
+    - name
+Optional:
+    - sku
+    - tags
+    - identity (block):
+        - identity_ids (required)
+        - type (required)
+EOT
+
+  type = map(object({
+    database_id = string
+    location    = string
+    name        = string
+    sku         = optional(string, "JA100")
+    tags        = optional(map(string))
+    identity = optional(object({
+      identity_ids = set(string)
+      type         = string
+    }))
+  }))
+}
+
