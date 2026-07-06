b9f01a777bc536f23c7003b74ec4c0af1f979c1c
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "mssql_job_agents" {
   description = "All mssql_job_agent resources"
   value       = azurerm_mssql_job_agent.mssql_job_agents
 }
+output "mssql_job_agents_database_id" {
+  description = "List of database_id values across all mssql_job_agents"
+  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.database_id]
+}
+output "mssql_job_agents_identity" {
+  description = "List of identity values across all mssql_job_agents"
+  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.identity]
+}
+output "mssql_job_agents_location" {
+  description = "List of location values across all mssql_job_agents"
+  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.location]
+}
+output "mssql_job_agents_name" {
+  description = "List of name values across all mssql_job_agents"
+  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.name]
+}
+output "mssql_job_agents_sku" {
+  description = "List of sku values across all mssql_job_agents"
+  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.sku]
+}
+output "mssql_job_agents_tags" {
+  description = "List of tags values across all mssql_job_agents"
+  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.tags]
+}
 
