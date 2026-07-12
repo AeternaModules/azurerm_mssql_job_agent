be378275f6d192a080f7857d7c8d7cae52440340
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:59 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "mssql_job_agents" {
-  description = "All mssql_job_agent resources"
-  value       = azurerm_mssql_job_agent.mssql_job_agents
-}
 output "mssql_job_agents_database_id" {
-  description = "List of database_id values across all mssql_job_agents"
-  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.database_id]
+  description = "Map of database_id values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.database_id }
 }
 output "mssql_job_agents_identity" {
-  description = "List of identity values across all mssql_job_agents"
-  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.identity]
+  description = "Map of identity values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.identity }
 }
 output "mssql_job_agents_location" {
-  description = "List of location values across all mssql_job_agents"
-  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.location]
+  description = "Map of location values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.location }
 }
 output "mssql_job_agents_name" {
-  description = "List of name values across all mssql_job_agents"
-  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.name]
+  description = "Map of name values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.name }
 }
 output "mssql_job_agents_sku" {
-  description = "List of sku values across all mssql_job_agents"
-  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.sku]
+  description = "Map of sku values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.sku }
 }
 output "mssql_job_agents_tags" {
-  description = "List of tags values across all mssql_job_agents"
-  value       = [for k, v in azurerm_mssql_job_agent.mssql_job_agents : v.tags]
+  description = "Map of tags values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.tags }
 }
 
