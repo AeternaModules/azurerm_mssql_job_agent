3a294ebd3f3a1786dad56fff8bc506a5853778c0
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "mssql_job_agents_id" {
   description = "Map of id values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
-  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.id }
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "mssql_job_agents_database_id" {
   description = "Map of database_id values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
-  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.database_id }
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.database_id if v.database_id != null && length(v.database_id) > 0 }
 }
 output "mssql_job_agents_identity" {
   description = "Map of identity values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
-  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.identity }
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.identity if v.identity != null && length(v.identity) > 0 }
 }
 output "mssql_job_agents_location" {
   description = "Map of location values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
-  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.location }
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "mssql_job_agents_name" {
   description = "Map of name values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
-  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.name }
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "mssql_job_agents_sku" {
   description = "Map of sku values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
-  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.sku }
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.sku if v.sku != null && length(v.sku) > 0 }
 }
 output "mssql_job_agents_tags" {
   description = "Map of tags values across all mssql_job_agents, keyed the same as var.mssql_job_agents"
-  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.tags }
+  value       = { for k, v in azurerm_mssql_job_agent.mssql_job_agents : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
